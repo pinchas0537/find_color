a440c10af12f7fec80622a375d001c7e527735d9
Pinchas Lifshitz <pnl8480537@gmail.com>
2026-02-23 18:12:30 +0200

    finish

--- src/Grid_layout.jsx
+++ src/Grid_layout.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+const clicks = []
 
 export default function grid_layout() {
+  const [count, setcount] = useState(0)
+  const [message, setMessage] = useState("")
+  const [status, setStatus] = useState("false")
   const board = []
   for (let i = 1; i <= 100; i++) {
     board.push(i)
   }
+  const cellsecret = useRef((Math.floor(Math.random() * 100) + 1))
+  function onClick(e, num) {
+    if (!clicks.includes(num)) {
+      clicks.push(num)
+      if (num !== cellsecret.current) {
+        setcount(count + 1)
+        e.target.className = "cell x"
+        setMessage("Keep Searching!")
+      }
+      else {
+        e.target.className = "cell v"
+        setMessage("Sucress")
+        setStatus("true")
+      }
+    }
+  }
+
   return (
     <div className='main'>
       <h1 className='titel'>Find the Secret Color!</h1>
       <div className='table'>
-      {board.map(num => <div key={num} className='cell'></div>)}
+        {board.map((num) =>
+          <div key={num} className='cell' onClick={(e) => {
+            if (status === "true") return
+            onClick(e, num)
+          }}>
+          </div>
+        )}
       </div>
-      <div className='click'>Clicks:</div>
+      <div className='click'>Clicks: {count}</div>
+      <div className={message === "Sucress" ? "suc" : "keep"}>{message}</div>
+      <button className={`${status}-button`} onClick={() => {
+        setMessage("")
+        setStatus("false")
+        setcount(0)
+
+      }}>now game</button>
     </div>
   )
 }
